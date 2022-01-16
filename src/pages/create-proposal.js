b81c561845ebec58f7558fd5fb56f65093d4d8ca
yibeibaoke <yibeibaoke@gmail.com>
2022-01-15 21:21:28 -0500

    fix create proposal

--- src/pages/create-proposal.js
+++ src/pages/create-proposal.js
@@ -16,17 +16,16 @@ import Market from '../../artifacts/contracts/NFTMarket.sol/NFTMarket.json'
 import GovernanceToken from '../../artifacts/contracts/PricerToken.sol/PricerToken.json'
 import Governor from '../../artifacts/contracts/PricerGovernor.sol/PricerGovernor.json'
 
-
 const styles = {
-	wrapper: {
-		marginTop: '120px',
-		marginBottom: '400px',
-		textAlign: 'center'
-	}
+     wrapper: {
+          marginTop: '120px',
+          marginBottom: '400px',
+          textAlign: 'center'
+     }
 }
 
 export default function CreateProposal() {
-	const { web3, Moralis, user } = useMoralis()
+  const { web3, Moralis, user } = useMoralis()
 	const [formInput, updateFormInput] = useState({ address: '', description: '' })
 	const router = useRouter()
 
@@ -35,76 +34,50 @@ export default function CreateProposal() {
 		const { address, description } = formInput
 		if (!address || !description) return
 
-		const web3Modal = new Web3Modal()
+    const web3Modal = new Web3Modal()
 		const connection = await web3Modal.connect()
 		const provider = new ethers.providers.Web3Provider(connection)
 		const signer = provider.getSigner()
 
-    		const governorContract = new ethers.Contract(mumbaiGovernorAddress, Governor.abi, signer)
-    		const governanceTokenContract = new ethers.Contract(mumbaiGovernorTokenAddress, GovernanceToken.abi, signer)
-    		const marketContract = new ethers.Contract(mumbaiNFTMarketAddress, Market.abi, signer)
+    const governorContract = new ethers.Contract(mumbaiGovernorAddress, Governor.abi, signer)
+    const governanceTokenContract = new ethers.Contract(mumbaiGovernorTokenAddress, GovernanceToken.abi, signer)
+    const marketContract = new ethers.Contract(mumbaiNFTMarketAddress, Market.abi, signer)
 
-    		console.log('current weight is: ' + await governanceTokenContract.getVotes(signer.getAddress()))
+    console.log("current weight is: " + await governanceTokenContract.getVotes(signer.getAddress()))
 
-    		const calldata = marketContract.interface.encodeFunctionData('updateRating', [mumbaiNFTAddress, 1])
+    const calldata = marketContract.interface.encodeFunctionData('updateRating', [mumbaiNFTAddress, 1])
 
-    		const proposal_tx = await governorContract.propose(
-       		 	[mumbaiNFTMarketAddress],
-        		[0],
-        		[calldata],
-        		description
-    		)
+    const proposal_tx = await governorContract.propose(
+        [mumbaiNFTMarketAddress],
+        [0],
+        [calldata],
+        description
+    );
 
-    		governorContract.on('ProposalCreated', (proposalId) => {
-        		console.log('got proposal id: ' + proposalId)
-        		const Proposal = Moralis.Object.extend('Proposals')
-        		const proposal = new Proposal()
+    governorContract.on("ProposalCreated", (proposalId) => {
+        console.log("got proposal id: " + proposalId);
+        const Proposal = Moralis.Object.extend("Proposals");
+        const proposal = new Proposal();
 
-        		proposal.set('proposal_id', proposalId)
-       	 		proposal.set('proposal_collection', address)
-        		proposal.set('proposal_description', description)
-        		proposal.set('proposal_state', '')
-        		proposal.set('proposal_rating', 0)
+        proposal.set("proposal_id", proposalId);
+        proposal.set("proposal_collection", address);
+        proposal.set("proposal_description", description);
+        proposal.set("proposal_state", "");
+        proposal.set("proposal_rating", 0);
 
-        		proposal.save()
-    		})
+        proposal.save();
+    });
 
-    		const Collection = Moralis.Object.extend('Collections')
-    		const collection = new Collection()
+    const Collection = Moralis.Object.extend("Collections");
+    const collection = new Collection();
 
-    		collection.set('collection_address', address)
-    		collection.set('nolvety_score', 7.5)
-    		collection.set('ai_tag', 'sketch')
+    collection.set("collection_address", address);
+    collection.set("nolvety_score", "7.5");
+    collection.set("ai_tag", "sketch");
 
-    		collection.save()
+    collection.save();
 
-    		console.log('end')
-	}
-
-	// eslint-disable-next-line no-unused-vars
-	async function createSale(url) {
-		const web3Modal = new Web3Modal()
-		const connection = await web3Modal.connect()
-		const provider = new ethers.providers.Web3Provider(connection)
-		const signer = provider.getSigner()
-
-		/* next, create the item */
-		let contract = new ethers.Contract(mumbaiNFTAddress, NFT.abi, signer)
-		let transaction = await contract.createToken(url)
-		let tx = await transaction.wait()
-		let event = tx.events[0]
-		let value = event.args[2]
-		let tokenId = value.toNumber()
-		const price = ethers.utils.parseUnits(formInput.price, 'ether')
-
-		/* then list the item for sale on the marketplace */
-		contract = new ethers.Contract(mumbaiNFTMarketAddress, Market.abi, signer)
-		// let listingPrice = await contract.getListingPirce()
-		// listingPrice = listingPrice.toString()
-
-		transaction = await contract.createMarketItem(mumbaiNFTAddress, tokenId, price, { value: 0 })
-		await transaction.wait()
-		router.push('/')
+    console.log("end")
 	}
 
 	return (
@@ -112,13 +85,15 @@ export default function CreateProposal() {
 			<div>
 				<input
 					placeholder="Collection address"
+					className="mt-8 border rounded p-4"
 					onChange={e => updateFormInput({ ...formInput, address: e.target.value })}
 				/>
 				<textarea
 					placeholder="Proposal Description"
+					className="mt-2 border rounded p-4"
 					onChange={e => updateFormInput({ ...formInput, description: e.target.value })}
 				/>
-				<button onClick={createCollectionProposal}>
+				<button onClick={createCollectionProposal} className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg">
           Create Proposal
 				</button>
 			</div>
